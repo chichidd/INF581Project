8b065c2b644ffb3c6d31f9faed2dead25191cc1b
chichidd <43724160+chichidd@users.noreply.github.com>
2020-03-20 11:16:00 +0100

    Update run.py

--- run.py
+++ run.py
@@ -10,9 +10,7 @@ from dqn.evaluate import evaluate_dqn
 
 def get_trainer(model_type, params):
     model_path = 'models/' + model_type + '.pt'
-    if model_type == 'a2c':
-        return A2CTrainer(params, model_path)
-    elif model_type == 'a3c':
+    if model_type == 'a3c':
         return A3CTrainer(params, model_path)
     elif model_type == 'dqn':
         return DQNTrainer(params, model_path)
@@ -24,15 +22,6 @@ def run_training(model_type):
     trainer = get_trainer(model_type, params)
     trainer.run()
 
-
-def run_inference(model_type):
-    params = Params('params/' + model_type + '.json')
-    if model_type == 'dqn':
-        score = dqn_inference('models/' + model_type + '.pt')
-    else:
-        score = actor_critic_inference(params, 'models/' + model_type + '.pt')
-
-    print('Total score: {0:.2f}'.format(score))
 
 
 def run_evaluation(model_type):
@@ -48,20 +37,17 @@ def run_evaluation(model_type):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--model', required=True,
-                        choices=['a2c', 'a3c', 'dqn'],
+                        choices=['a3c', 'dqn'],
                         help='Which model to run / train.')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--train', action='store_true',
                        help='Train model.')
-    group.add_argument('--inference', action='store_true',
-                       help='Model inference.')
     group.add_argument('--evaluate', action='store_true',
                        help='Evaluate model on 100 episodes.')
 
     args = vars(parser.parse_args())
     if args['train']:
         run_training(args['model'])
-    elif args['evaluate']:
-        run_evaluation(args['model'])
     else:
-        run_inference(args['model'])
+        run_evaluation(args['model'])
+
